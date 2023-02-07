135fc9217edf2bd64d0371a90cd07ef9d381fb85
Jo Franz <franz@systopia.de>
2023-02-07 14:24:50 +0100

    civix upgrade 22.12.1

--- mods.php
+++ mods.php
@@ -94,15 +94,6 @@ function mods_civicrm_validateForm($formName, &$fields, &$files, &$form, &$error
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function mods_civicrm_xmlMenu(&$files) {
-  _mods_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -112,96 +103,12 @@ function mods_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
- */
-function mods_civicrm_postInstall() {
-  _mods_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function mods_civicrm_uninstall() {
-  _mods_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
 function mods_civicrm_enable() {
   _mods_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function mods_civicrm_disable() {
-  _mods_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function mods_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _mods_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function mods_civicrm_managed(&$entities) {
-  _mods_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function mods_civicrm_caseTypes(&$caseTypes) {
-  _mods_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
- */
-function mods_civicrm_angularModules(&$angularModules) {
-  _mods_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function mods_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _mods_civix_civicrm_alterSettingsFolders($metaDataFolders);
 }
 
 /**
@@ -332,7 +239,6 @@ function mods_civicrm_pre($op, $objectName, $id, &$params) {
     CRM_Mods_CardTitle::showCardTitleShouldBeAdjustedWarning($id, $params);
   }
 }
-
 
 /**
  * Implements hook_civicrm_post
